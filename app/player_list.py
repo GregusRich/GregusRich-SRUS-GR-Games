c46726a75eae68e4d2d973f0bb8ab452580de185
GregusRich <141307499+GregusRich@users.noreply.github.com>
2024-07-23 16:31:57 +0800

    Added player list class with initialiser.

---
+++ app/player_list.py
@@ -0,0 +1,13 @@
+class PlayerList:
+    """
+    A class to represents a list of players
+    """
+    def __init__(self):
+        self._head = None  # Initialise the head of the list to None.
+
+
+
+
+
+
+
