97ca1147f1953e7a44603e95db8803c066c01845
richag <gregory.richards87@gmail.com>
2024-08-02 11:58:10 +0800

    Added PlayerList class.

--- test/player_test.py
+++ test/player_test.py
@@ -9,8 +9,13 @@ Unit tests for the player class.
 class TestPlayer(unittest.TestCase):
     def test_uid_property(self):
         player = Player("12345", "Greg")
+        player2 = Player(unique_id="987654", player_name="Tom")
         self.assertEqual(player.uid, "12345")
+        self.assertEqual(player2.uid, second="987654")
 
     def test_name_property(self):
         player = Player("12345", "Greg")
+        player2 = Player(unique_id="987654", player_name="Tom")
         self.assertEqual(player.name, "Greg")
+        self.assertEqual(player2.name, second="Tom")
+
